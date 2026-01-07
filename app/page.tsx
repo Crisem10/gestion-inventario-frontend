4adc9676c7e9cf6e288bf8b87bd3219b2250dd02
Cpinto1234 <Carolinapintomb@gmail.com>
2026-01-07 11:28:20 -0500

    3 commit

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,7 @@ async function getDashboardStats(): Promise<DashboardStats> {
   }
 }
 
+export const dynamic = 'force-dynamic'
 export default async function DashboardPage() {
   const stats = await getDashboardStats()
 
